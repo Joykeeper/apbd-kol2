35c3081993d6f3a05d4bad11eba2ea3b14749724
Joykeeper <vladyslav.edov@gmail.com>
2025-06-04 08:25:43 +0200

    Starting

---
+++ Kol2Preparation/Data/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Kol2Preparation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kol2Preparation.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+    
+    // public DbSet<Pastry> Pastries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // modelBuilder.Entity<OrderPastry>().ToTable("Order_Pastry");
+        
+        // modelBuilder.Entity<Employee>().HasData(new List<Employee>
+        // {
+        //     new Employee {
+        //         Id = 1,
+        //         FirstName = "Adam",
+        //         LastName = "Nowak"
+        //     },
+        //     new Employee {
+        //         Id = 2,
+        //         FirstName = "Aleksandra",
+        //         LastName = "Wiśniewska"
+        //     }
+        // });
+    }
+}
---
+++ Kol2Preparation/Program.cs
@@ -0,0 +1,21 @@
+using Kol2Preparation.Data;
+using Kol2Preparation.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDbService, DbService>();
+
+
+var app = builder.Build();
+
+app.MapControllers();
+
+
+app.Run();
