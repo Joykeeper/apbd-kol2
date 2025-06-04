63d9459f60b2936ecd47494211a62c4615992b31
Vladyslav Dovzhenko <s30918@pjwstk.edu.pl>
2025-06-04 17:18:46 +0200

    Last addition

--- Kol2Preparation/Exceptions/MockException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Kol2Preparation.Exceptions;
-
-public class MockException() : Exception("MockException's message")
-{
-    
-}
--- Kol2Preparation/Services/IDbService.cs
+++ Kol2Preparation/Services/IDbService.cs
@@ -4,9 +4,6 @@ namespace Kol2Preparation.Services;
 
 public interface IDbService
 {
-    // possible methods
-    // Task AddPrescription(PrescriptionDto prescription);
-    // Task<PatientDto> GetPatientData(int patientId);
     Task<object> GetCustomerData(int id);
     Task AddCustomerData(DataDto customer);
     Task<Boolean> DoesConcertExist(string concert);
